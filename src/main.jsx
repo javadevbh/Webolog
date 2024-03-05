1804b37ee04d71864f2c1583466e9520ee2e7e1a
Javad Bahrami <javadev14bh@gmail.com>
2024-03-05 18:03:21 +0330

    Install and configuration GraphQL

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: "https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clta3dq8h29r807uznm3trep2/master",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>
+);
