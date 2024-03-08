acc6b4a9e30049d3cfa1fe727383687f9c3e30b7
Javad Bahrami <javadev14bh@gmail.com>
2024-03-08 14:24:07 +0330

    Add .env file

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import theme from "./mui/theme.js";
 import { ThemeProvider } from "@mui/material";
 
 const client = new ApolloClient({
-  uri: "https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clta3dq8h29r807uznm3trep2/master",
+  uri: import.meta.env.VITE_URI,
   cache: new InMemoryCache(),
 });
 
