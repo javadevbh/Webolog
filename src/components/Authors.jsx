f26b64663265706c32337e07986874c02e9aff40
Javad Bahrami <javadev14bh@gmail.com>
2024-03-09 16:31:55 +0330

    Add get authors query and authors component

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
+
+//GraphQL
+import { useQuery } from "@apollo/client";
+import { GET_AUTHORS_INFO } from "../graphql/queries";
 
 function Authors() {
+  const { loading, data, error } = useQuery(GET_AUTHORS_INFO);
+  if (loading) return <h4>Loading...</h4>;
+  if (error) return <h4>{error.message}</h4>;
   return (
-    <div>Authors</div>
-  )
+    <Grid
+      container
+      sx={{ boxShadow: "rgba(0 , 0 , 0 , 0.1) 0 4px 12px", borderRadius: 4 }}
+    >
+      {data.authors.map((author, index) => (
+        <React.Fragment key={author.id}>
+          <Grid item xs={12} padding={2} display="flex" alignItems="center">
+            <Avatar src={author.avatar.url} sx={{ marginLeft: 1 }} />
+            <Typography component="p" variant="p" color="text.secondary">
+              {author.name}
+            </Typography>
+          </Grid>
+          {index !== data.authors.length - 1 && (
+            <Grid item xs={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </React.Fragment>
+      ))}
+    </Grid>
+  );
 }
 
-export default Authors
+export default Authors;
